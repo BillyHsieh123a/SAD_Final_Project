2d10c9b3ab6449664efc23be1299c9af3d55612f
william25885 <william25885@gmail.com>
2024-12-05 03:02:51 +0800

    Update login.py

--- backend/login.py
+++ backend/login.py
@@ -3,3 +3,37 @@ from db import psql_conn
 
 
 login = Blueprint("login", __name__)
+
+@login.route('/login', methods = 'POST')
+def handle_login():
+    if request.method == 'POST':
+        # check email or phone
+        accountInput = request.form.get('email')
+        password = request.form.get('password')
+        cmd = """"""
+
+        # phone num
+        if accountInput.isdigit():
+            cmd = """
+            select user.phone, user.email, user.password
+            from public.user
+            where user.phone = %s and user.password = %s
+            """
+
+        else:
+            cmd = """
+            select user.phone, user.email, user.password
+            from public.user
+            where user.email = %s and user.password = %s
+            """
+
+    psql_conn.execute(cmd, [accountInput, password])
+
+    rows = psql_conn.fetchall()
+    
+    # get no user
+    if len(rows) == 0:
+        return jsonify(0)
+    else:
+        return jsonify(1)
+    
