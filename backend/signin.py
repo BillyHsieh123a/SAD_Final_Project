21d192c37985a26577a137b4ce7ba6d526b67f35
Barry1749 <146699243+Barry1749@users.noreply.github.com>
2024-12-11 16:07:42 +0800

    Update signin.py

---
+++ backend/signin.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, jsonify, request, session, render_template, url_for
+from db import get_psql_conn
+
+signin = Blueprint("signin", __name__)
+
+# return cloth name, price, image url, cloth_id, color
+@signin.route('/signin_', methods=['POST'])
+def signin():
+    psql_conn = get_psql_conn()
+    if psql_conn is not None:
+        cur = psql_conn.cursor()
+    else:
+        print("Failed to connect to the database.")
+
+    data = request.json
+    fname = data.get('fname')
+    lname = data.get('lname')
+    password = data.get('password')
+    phone = data.get('phone')
+    email = data.get('email')
+    bdate = data.get('bdate')
+    gender = data.get('gender')
+
+    cur.execute(
+        '''
+        INSERT INTO public."user" (fname, lname, phone, email, bdate, gender, role)
+        VALUES(%s, %s, %s, %s, %s, %s, %s, U)
+        ''',
+        (fname, lname, password, phone, email, bdate, gender)
+    )
+
+    psql_conn.commit()
+    return jsonify({"message": "successfully registered!"}), 200
