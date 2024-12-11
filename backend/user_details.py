bf5c6bb02cc94222cfe038c6587f6176b572cf5a
Barry1749 <146699243+Barry1749@users.noreply.github.com>
2024-12-11 15:08:30 +0800

    Revert "Merge branch 'main' of https://github.com/BillyHsieh123a/113_1_Web_App_Final_Project"
    This reverts commit 0929c3fcd6c7ede7000e1718cb298d43357792bd, reversing
    changes made to 07fa73dd5fd3beea8de3bcae7d53c45758b4596a.

--- backend/user_details.py
+++ backend/user_details.py
@@ -1,21 +1,17 @@
 from flask import Blueprint, jsonify, request, session, render_template, url_for
-from db import psql_conn
+from db import get_psql_conn
 
 user_details = Blueprint("user_details", __name__)
 
 # return cloth name, price, image url, cloth_id, color
 @user_details.route('/user_details_load_user_data', methods=['GET'])
 def user_details_load_user_data():
-<<<<<<< HEAD
-    cur = psql_conn.cursor()
-=======
     psql_conn = get_psql_conn()
     if psql_conn is not None:
         cur = psql_conn.cursor()
     else:
         print("Failed to connect to the database.")
 
->>>>>>> 59a943d478219616fd042bef90997088b40df8ad
     user_id = request.args.get('user_id')
 
     cur.execute(
